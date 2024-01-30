f3019cbf8a5d21872af07cf33bc37c648bb962e4
ManuelDiLena <manuel.dilena.29@gmail.com>
2024-01-30 18:42:43 +0100

    Form validation

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // MENU RESPONSIVE
-let btnMenuOpen = document.getElementById('btnMenuOpen');
-    btnMenuClose = document.getElementById('btnMenuClose');
-    menuResponsive = document.getElementById('barMenu');
+let btnMenuOpen = document.getElementById('btnMenuOpen'),
+    btnMenuClose = document.getElementById('btnMenuClose'),
+    menuResponsive = document.getElementById('barMenu'),
     links = document.getElementById('links');
 
     // Click Open
@@ -21,8 +21,8 @@ let btnMenuOpen = document.getElementById('btnMenuOpen');
     });
 
 // PRODUCTS SLIDER
-let container = document.querySelector('.slider');
-    btnLeft = document.getElementById('btnLeft');
+let container = document.querySelector('.slider'),
+    btnLeft = document.getElementById('btnLeft'),
     btnRight = document.getElementById('btnRight');
 
     // Right btn event
@@ -34,3 +34,41 @@ let container = document.querySelector('.slider');
     btnLeft.addEventListener('click', function() {
       container.scrollLeft -= container.offsetWidth;
     });
+
+// FORM VALIDATION
+let form = document.getElementById('form');
+
+function validation(e) {
+  let inputName = document.getElementById('name'),
+      inputEmail = document.getElementById('email'),
+      inpputComments = document.getElementById('comments'),
+      alertSuccess = document.getElementById('alertSuccess'),
+      alertError = document.getElementById('alertError');
+
+  if (inputName.value == 0 || inputEmail.value == 0 || inpputComments.value == 0) {
+    e.preventDefault();
+    alertError.classList.remove('hide');
+    alertError.classList.add('show');
+
+    setTimeout(function() {
+      alertError.classList.remove('show');
+      alertError.classList.add('hide');
+    }, 2000);
+
+  } else {
+    e.preventDefault();
+    alertSuccess.classList.remove('hide');
+    alertSuccess.classList.add('show');
+
+    setTimeout(function() {
+      alertSuccess.classList.remove('show');
+      alertSuccess.classList.add('hide');
+    }, 2000);
+    inputName.value = "";
+    inputEmail.value = "";
+    inpputComments.value = "";
+  }
+}
+
+// Form events
+form.addEventListener('submit', validation);
