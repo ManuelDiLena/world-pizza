420e0d7feac54f07cb0ee6a7c6f5d7554c4e3588
ManuelDiLena <manuel.dilena.29@gmail.com>
2024-01-30 18:24:20 +0100

    Menu responsive

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// MENU RESPONSIVE
+let btnMenuOpen = document.getElementById('btnMenuOpen');
+    btnMenuClose = document.getElementById('btnMenuClose');
+    menuResponsive = document.getElementById('barMenu');
+    links = document.getElementById('links');
+
+    // Click Open
+    btnMenuOpen.addEventListener('click', function() {
+      menuResponsive.classList.add('active');
+    });
+
+    // Click Close
+    btnMenuClose.addEventListener('click', function() {
+      menuResponsive.classList.remove('active');
+    });
+
+    // Close menu with link elements
+    links.addEventListener('click', function() {
+      menuResponsive.style.transitionDelay = '0.5s';
+      menuResponsive.classList.remove('active');
+    });
