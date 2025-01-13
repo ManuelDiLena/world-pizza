72138ee913fc76e2db040e083bd02fbac43dfe2e
ManuelDiLena <manuel.dilena.29@gmail.com>
2025-01-13 18:47:05 +0100

    Contact section

--- js/app.js
+++ js/app.js
@@ -35,3 +35,41 @@ let container = document.querySelector('.slider'),
     container.scrollLeft -= container.offsetWidth;
   });
 
+// Form validation
+let form = document.getElementById('form');
+
+function validation(e) {
+  let inputName = document.getElementById('name'),
+      inputEmail = document.getElementById('email'),
+      inputComments = document.getElementById('comments'),
+      alertSuccess = document.getElementById('alertSuccess'),
+      alertError = document.getElementById('alertError');
+
+  if (inputName.value == 0 || inputEmail.value == 0 || inputComments.value == 0) {
+    e.preventDefault();
+    alertError.classList.remove('hide');
+    alertError.classList.add('show');
+
+    setTimeout(function() {
+      alertError.classList.remove('show');
+      alertError.classList.add('hide');
+    }, 2000);
+  } else {
+    e.preventDefault();
+    alertSuccess.classList.remove('hide');
+    alertSuccess.classList.add('show');
+
+    setTimeout(function() {
+      alertSuccess.classList.remove('show');
+      alertSuccess.classList.add('hide');
+    }, 2000);
+
+    inputName.value = "";
+    inputEmail.value = "";
+    inputComments.value = "";
+  }
+}
+
+// Form events
+form.addEventListener('submit', validation);
+
